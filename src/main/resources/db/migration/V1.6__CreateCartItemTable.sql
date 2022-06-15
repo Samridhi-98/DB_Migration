9bd053abca8e05685c1340628cadc51ca3e3e681
Samriddhi <agrawalsamriddhi83@gmail.com>
2022-06-15 21:51:55 +0530

    Corrected the cart item query

--- src/main/resources/db/migration/V1.6__CreateCartItemTable.sql
+++ src/main/resources/db/migration/V1.6__CreateCartItemTable.sql
@@ -3,5 +3,6 @@ CREATE TABLE cart_items(
     cart_id INT NOT NULL,
     product_id INT NOT NULL,
     quantity INT NOT NULL,
-    FOREIGN KEY (cart_id) REFERENCES carts(id)
+    FOREIGN KEY (cart_id) REFERENCES carts(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
